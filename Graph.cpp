2e991abd3a9e9ccc1a4d7085ea8dad7e680ec123
Isaac Fuenmayor <isaacfuenmayora@gmail.com>
2021-12-08 17:16:02 -0500

    Add Edge Creation Functionality

--- Graph.cpp
+++ Graph.cpp
@@ -9,12 +9,17 @@ void Graph::setCurrNode(int id) {
     currNode = adjList[id];
 }
 
-void Graph::insertNode(int id, string title, bool outgoing) {
-    if(outgoing)
-        currNode->outgoingNodes.push_back(new Node(id, title, currNode));
-    //TODO: implement incomingNodes
-    // else
-    //     currNode->incomingNodes.push_back(new Node(id, title));
+void Graph::connectOutNode(int id) {
+    currNode->outgoingNodes.push_back(adjList[id]);
+}
+
+void Graph::insertOutNode(int id, string title) {
+    currNode->outgoingNodes.push_back(new Node(id, title, currNode));
+}
+
+void Graph::breadthFirstSearch(int targetID) {
+    queue<Node*> nodes({currNode});
+    //TODO: finish this
 }
 
 Graph::Node::Node(int id, string title, Node* prev)
