6b8f42a72227fb67b284754ff8e05798ab163992
Akanksh Jain <aka200219@gmail.com>
2021-12-08 18:11:34 -0500

    Updated name for Wiki API

--- WikiAPI.h
+++ WikiAPI.h
@@ -1,11 +1,11 @@
 #include <string>
 using namespace std;
 
-class ApiWrapper{
+class WikiAPI{
 private:
     string wiki, search_url, links_url, thumbnail_url;
 public:
-    ApiWrapper(string w){
+    WikiAPI(string w){
         wiki=w;
         search_url=wiki+"api.php?format=json&formatversion=2&action=query&generator=prefixsearch&gpslimit=10&prop=pageimages|pageterms&piprop=thumbnail&pithumbsize=50&pilimit=10&pilicense=any&wbptterms=description&redirects=&gpssearch=";
         links_url=wiki+"api.php?format=json&formatversion=2&action=query&generator=links&gpllimit=500&gplnamespace=0&redirects=&pageids=";
